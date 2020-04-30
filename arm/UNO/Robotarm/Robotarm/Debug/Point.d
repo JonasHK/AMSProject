61a64f8f520a5e256ee44393fd84a2a7d3a6c347
Jonas <jonas.h.kristensen@gmail.com>
2020-04-30 08:05:24 +0200

    removed /Debug

--- arm/UNO/Robotarm/Robotarm/Debug/Point.d
+++
@@ -1,6 +0,0 @@
-Point.d Point.o: .././Point.cpp .././Point.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
-
-.././Point.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
--- arm/UNO/Robotarm/Robotarm/Debug/armGeometry.d
+++
@@ -1,6 +0,0 @@
-armGeometry.d armGeometry.o: .././armGeometry.cpp .././armGeometry.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
-
-.././armGeometry.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
