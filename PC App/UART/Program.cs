ceb9a990bb16b05a411dd58f62d9429c0f838cea
Troels Bleicken <troels_bleicken@hotmail.com>
2020-05-18 09:36:01 +0200

    Added pc app

---
+++ PC App/UART/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using InTheHand.Net.Bluetooth;
+
+namespace UART
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            SerialPort bt = new SerialPort("COM7", 9600, Parity.None, 8, StopBits.One);
+            SerialPort arduino = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+            bt.Open();
+            arduino.Open();
+            Console.CursorVisible = false;
+            
+
+            while (true)
+            {
+                char data = Convert.ToChar(arduino.ReadChar());
+                bt.Write(data.ToString());
+                
+                Console.Write(data);
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);//Reset console coursor so the output is always in the same place
+                Console.CursorVisible = false;                                      // (purely for the looks)
+            }
+        }
+
+    }
+}
